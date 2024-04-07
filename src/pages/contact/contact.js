c76a2fc973d1d59afc5fff1c518ae31522bd3920
Schvartzegreg <gregoryerrl@gmail.com>
2024-04-07 16:06:36 +0800

    ui fix

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -4,9 +4,9 @@ import "./contact.css";
 const Contact = () => {
   return (
     <div className="d-flex justify-content-center vh-100">
-      <div class="bg mt-5">
-        <h2 className="text-center mb-4 py-3">Contacts / Social Media</h2>
-        <div className="mb-3 all">
+      <div class="bg mt-5 mb-5">
+        <h2 className="text-center mb-5 py-3">Contacts / Social Media</h2>
+        <div className="mb-3 mt-5 all">
           <i className="bi bi-github" /> <a>gregoryerrl</a>
         </div>
         <div className="mb-3 all">
